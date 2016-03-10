dc3d785cdda54cc39e4b8ef865d23ad8c6c5fa6f
wangyan <wyvernnot@gmail.com>
2016-03-10 10:50:45 +0800

    fix table

--- scripts/generate_readme.js
+++ scripts/generate_readme.js
@@ -1,5 +1,5 @@
 console.log('|名字|HEX|RGB|HSV|HSL|CMYK|');
-console.log('|--|--|--|--|--|--|');
+console.log('|---|---|---|---|---|---|');
 require('../').forEach(item=>{
   console.log(`|${item.name}|${item.HEX}|${item.RGB}|${item.HSV}|${item.HSL}|${item.CMYK}|`);
 })
